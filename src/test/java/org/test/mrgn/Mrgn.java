51e2b946f7dfba0f70e8d7cc10447cfb186b8e31
dinesh <dineshkumar.b6593@gmail.com>
2020-05-25 09:54:19 +0530

    branch2

--- src/test/java/org/test/mrgn/Mrgn.java
+++ src/test/java/org/test/mrgn/Mrgn.java
@@ -5,7 +5,8 @@ public class Mrgn {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("world");
-
+		System.out.println("branch");
+		System.out.println("branch2");
 	}
 
 }
